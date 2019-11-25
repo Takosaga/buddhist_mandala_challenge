957b5d27e433205300c927897c926d4edfdac41b
Gonzalo Gamez <32805901+Takosaga@users.noreply.github.com>
2019-11-24 22:52:08 -0600

    adding functions for outer cirlce and moving up

--- buddhist_mandala.py
+++ buddhist_mandala.py
@@ -4,6 +4,25 @@ import turtle
 screen = turtle.Screen()
 screen.title("Buddhist Mandala")
 tracy = turtle.Turtle()
+screen.bgcolor("black")
 
+#outer cirlce of the mandala
+def outer_circle():
+    tracy.pendown()
+    tracy.color("white")
+    tracy.circle(300)
+
+#moving up without leaving marks
+def move_up():
+    tracy.penup()
+    tracy.left(90)
+    tracy.forward(30)
+    tracy.right(90)
+
+#calling all functions
+tracy.penup()
+tracy.setposition(0,-300)
+outer_circle()
+move_up()
 
 turtle.done()
