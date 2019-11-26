af9dad8133237576f79a35bf9da6568f2cff06ac
Gonzalo Gamez <32805901+Takosaga@users.noreply.github.com>
2019-11-26 09:27:24 -0600

    Added rainbow circle

--- buddhist_mandala.py
+++ buddhist_mandala.py
@@ -9,20 +9,45 @@ screen.bgcolor("black")
 #outer cirlce of the mandala
 def outer_circle():
     tracy.pendown()
+    tracy.pensize(15)
     tracy.color("white")
-    tracy.circle(300)
+    tracy.circle(280)
 
 #moving up without leaving marks
 def move_up():
     tracy.penup()
     tracy.left(90)
-    tracy.forward(30)
+    tracy.forward(20)
     tracy.right(90)
+
+#making rainbow circle 
+def rainbow_first_circle():
+    color_change = 60
+    tracy.pendown()
+    tracy.pensize(15)
+    for i in range(color_change):
+        if i % 5 == 0:
+            tracy.color("blue")
+            tracy.circle(260,360/color_change,color_change)
+        elif i % 5 == 1:
+            tracy.color("cyan")
+            tracy.circle(260,360/color_change,color_change)
+        elif i % 5 == 2:
+            tracy.color("green")
+            tracy.circle(260,360/color_change,color_change)
+        elif i % 5 == 3:
+            tracy.color("red")
+            tracy.circle(260,360/color_change,color_change)
+        else:
+            tracy.color("orange")
+            tracy.circle(265,360/color_change,color_change)
 
 #calling all functions
 tracy.penup()
-tracy.setposition(0,-300)
+tracy.speed(0)
+tracy.setposition(0,-280)
 outer_circle()
 move_up()
+rainbow_first_circle()
 
 turtle.done()
