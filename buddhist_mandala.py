80db7f043ad2859e08d2f151b87b68022fe08d10
Gonzalo Gamez <32805901+Takosaga@users.noreply.github.com>
2019-12-02 21:31:37 -0600

    adding more circle square mark accents

--- buddhist_mandala.py
+++ buddhist_mandala.py
@@ -103,7 +103,6 @@ def inner_circle_deco():
 def circle_square_marks():
     circle_angle = math.degrees(math.asin(50/200))*2
     square_angle = (90 - circle_angle)/2
-    print(circle_angle)
     tracy.pensize(2)
     tracy.color("black")
     for i in range(4):
@@ -113,6 +112,30 @@ def circle_square_marks():
         tracy.circle(200,circle_angle)
         tracy.penup()
         tracy.circle(200,square_angle)
+
+#marking circles with red accent in middle
+def circle_square_marks_second():
+    circle_angle = math.degrees(math.asin(50/170))*2
+    square_angle = (90 - circle_angle)/2
+    tracy.pensize(2)
+    tracy.color("black")
+    for i in range(4):
+        tracy.penup()
+        tracy.circle(170,square_angle)
+        tracy.pendown()
+        tracy.circle(170,circle_angle/3)
+        tracy.color("red")
+        tracy.right(90)
+        tracy.forward(15)
+        tracy.left(90)
+        tracy.circle(185,circle_angle/3)
+        tracy.left(90)
+        tracy.forward(15)
+        tracy.right(90)
+        tracy.color("black")
+        tracy.circle(170,circle_angle/3)
+        tracy.penup()
+        tracy.circle(170,square_angle)
 
 #calling all functions
 tracy.penup()
@@ -130,5 +153,7 @@ white_cricle_accents()
 move_up(45)
 inner_circle_deco()
 circle_square_marks()
+move_up(30)
+circle_square_marks_second()
 
 turtle.done()
